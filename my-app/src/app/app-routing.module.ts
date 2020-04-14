a4663f183c2ef2f33d00874b7df914e94d298c28
dahlianoom <43912304+dahlianoom@users.noreply.github.com>
2020-04-14 19:41:09 +0300

    Add routes
    add routes for homepage and signup page

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { OpportunitiesListComponent } from './components/opportunities-list/opportunities-list.component';
+import { MainInfoComponent } from './components/main-info/main-info.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { IntroComponent } from './components/intro/intro.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IntroComponent},
+  {path: 'signup', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
