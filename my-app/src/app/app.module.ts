2f909177b93a93f436c92d786dfe28470612483a
dahlianoom <43912304+dahlianoom@users.noreply.github.com>
2020-04-15 21:13:04 +0300

    gathering data
    add gathering data from forms in sign-up component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -23,7 +24,9 @@ import { IntroComponent } from './components/intro/intro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/components/sign-up/sign-up.component.ts
+++ my-app/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,13 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
  
-  constructor() { }
+  checkoutForm : FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
   registrationState = 0; // 0 - student, 1 - organization
 
   changeHandler(event) {
     this.registrationState = event;
   }
   ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm () {
+    if (this.registrationState == 0)  {
+    this.checkoutForm = this.formBuilder.group({
+      fio: ['', [
+        Validators.required
+      ]
+      ],
+      faculty: ['', [
+        Validators.required
+      ]
+      ],
+      group: ['', [
+        Validators.required
+      ]
+      ],
+      tel: ['', [
+        Validators.required
+      ]
+      ],
+      portfolio: ['', [
+        Validators.required
+      ]
+      ],
+      contacts: ['', [
+        Validators.required
+      ]
+      ],
+      mail: ['', [
+      Validators.required,
+      Validators.email
+      ]
+      ],
+      pass: ['', [
+        Validators.required
+      ]
+      ]
+      
+    });
+    }
+    else if (this.registrationState == 1)  {
+      this.checkoutForm = this.formBuilder.group({
+        name: ['', [
+          Validators.required
+        ]
+        ],
+        description: ['', [
+          Validators.required
+        ]
+        ],
+        address: ['', [
+          Validators.required
+        ]
+        ],
+        tel: ['', [
+          Validators.required
+        ]
+        ],
+        mail: ['', [
+          Validators.required,
+          Validators.email
+        ]
+        ],
+        pass: ['', [
+          Validators.required
+        ]
+        ]
+      });
+      }
+  }
+  onSubmit() {
+
+  }
 }
